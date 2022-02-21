3cd9fff1797702afb2b45384b0a0ce4c1f224f6a
akshith1212 <93110555+akshith1212@users.noreply.github.com>
2022-02-21 06:40:51 +0000

    done

---
+++ p2original.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void input_line(float *x1,float *y1)
+{
+printf("enter the coardinate values");
+  scanf("%f %f",x1,y1);
+}
+int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3)
+{
+  
+}
